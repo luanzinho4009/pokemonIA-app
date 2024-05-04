2eb61a69b25be1f91260ff263d24f0e100c0dfbf
luan <luan.santana@natahouse.com>
2024-05-04 11:45:38 -0300

    feat: initial commit configuration

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white">PokeIA app</Text>
+      <Text className="text-white">app da api do pokemon com chat-gpt</Text>
+    </View>
+  );
+}
